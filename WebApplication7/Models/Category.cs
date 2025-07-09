c82a51f5c35abb28e4cc59d6b8b740d99490b1cb
Mehdi Heidari <hymehdi84@gmail.com>
2025-07-09 21:15:02 +0200

    creating category class

---
+++ WebApplication7/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication7.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public string? CategoryDescription { get; set; }
+    public List<Pie> Pies { get; set; } 
+}
